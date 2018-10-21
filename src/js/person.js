d46526c4e4d061bb706b47fa7c0668a0dc592f97
Md Shahin Patowary <s.patowary@yahoo.com>
2018-10-21 20:27:30 +0200

    add tests for person objects

---
+++ src/js/person.js
@@ -0,0 +1,9 @@
+function Person(attr) {
+    this.weight = attr.weight;
+    this.height = attr.height;
+};
+
+Person.prototype.calculate_bmi = function() {
+    this.bmiValue = 26.01;
+    this.bmiMessage = "Overweight"
+  };
